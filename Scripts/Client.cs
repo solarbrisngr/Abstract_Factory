cb6e4a8ed0cf093d99aa642b7d7f182bd389938b
solarbrisngr <59808313+solarbrisngr@users.noreply.github.com>
2020-01-29 15:40:08 -0700

    Add files via upload

---
+++ Scripts/Client.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Client : MonoBehaviour
+{
+    public int NumberOfWheels;
+    public bool Engine;
+    public int Passengers;
+    public bool Cargo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        VehicleRequirements requirements = new VehicleRequirements();
+
+        requirements.NumberOfWheels = Mathf.Max(NumberOfWheels, 1);
+        requirements.Engine = Engine;
+        requirements.Passengers = Mathf.Max(Passengers);
+        requirements.Engine = Cargo;
+        requirements.HasCargo = Cargo;
+
+        //IVehicle v = GetVehicle(requirements);
+        VehicleFactory factory = new VehicleFactory(requirements);
+        IVehicle v = factory.Create();
+        Debug.Log(v);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //private static IVehicle GetVehicle(VehicleRequirements requirements)
+    //{
+    //    VehicleFactory factory = new VehicleFactory();
+    //    //IVehicle vehicle;
+
+    //    if (requirements.Engine)
+    //    {
+    //        return factory.MotorVehicleFactory().Create(requirements);
+    //    }
+
+    //    return factory.CycleFactory().Create(requirements);
+    //}
+}
---
+++ Scripts/VehicleFactory.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IVehicleFactory
+{
+    IVehicle Create(VehicleRequirements requirements);
+}
+
+//public abstract class AbstractVehicleFactory
+//{
+//    //public abstract IVehicleFactory CycleFactory();
+//    //public abstract IVehicleFactory MotorVehicleFactory();
+
+//    public abstract IVehicle Create();
+//}
+
+public class VehicleFactory // : AbstractVehicleFactory
+{
+    //public override IVehicleFactory CycleFactory()
+    //{
+    //    return new Cyclefactory();
+    //}
+
+    //public override IVehicleFactory MotorVehicleFactory()
+    //{
+    //    return new MotorVehicleFactory();
+    //}
+
+    private readonly IVehicleFactory _factory;
+    private readonly VehicleRequirements _requirements;
+    
+    public VehicleFactory(VehicleRequirements requirements)
+    {
+        _factory = requirements.Engine ? (IVehicleFactory)new MotorVehicleFactory() : new Cyclefactory();
+        _requirements = requirements;
+    }
+
+    public IVehicle Create()
+    {
+        return _factory.Create(_requirements);
+    }
+}
+
+public class Cyclefactory : IVehicleFactory
+{
+    public IVehicle Create(VehicleRequirements requirements)
+    {
+        switch (requirements.Passengers)
+        {
+            case 1:
+                if (requirements.NumberOfWheels == 1) return new Unicycle();
+                return new Bicycle();
+            case 2:
+                return new Tandem();
+            case 3:
+                return new Tricycle();
+            case 4:
+                if (requirements.HasCargo) return new GoKart();
+                return new FamilyBike();
+            default:
+                return new Bicycle();
+        }
+    }
+}
+
+public class MotorVehicleFactory : IVehicleFactory
+{
+    public IVehicle Create(VehicleRequirements requirements)
+    {
+        switch (requirements.Passengers)
+        {
+           case 2:
+                return new Motorbike();
+            default:
+                return new Truck();
+        }
+    }
+}
